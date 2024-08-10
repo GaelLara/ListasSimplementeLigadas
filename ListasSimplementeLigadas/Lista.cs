26c0fa3628e9c2d516ccb22a095179f554603683
Gael Lara <laragael2003@gmail.com>
2024-08-10 12:34:56 -0700

    Add project files.

---
+++ ListasSimplementeLigadas/Lista.cs
@@ -0,0 +1,41 @@
+﻿namespace ListasSimplementeLigadas
+{
+    internal class Lista
+    {
+        private Nodo _nodoIncial;
+        public Lista()
+        {
+            _nodoIncial = new Nodo();
+        }
+
+        private bool EstaVacia()
+        {
+            if (_nodoIncial.Enlace == null)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public void Vaciar()
+        {
+            _nodoIncial.Enlace = null;
+        }
+
+        public void Agregar(string valor)
+        {
+            Nodo nodoActual = _nodoIncial;
+            while (nodoActual.Enlace != null)
+            {
+                nodoActual = nodoActual.Enlace;
+            }
+
+            Nodo nodoNuevo = new Nodo(valor);
+            nodoActual.Enlace = nodoNuevo;
+        }
+    }
+}
+
---
+++ ListasSimplementeLigadas/Nodo.cs
@@ -0,0 +1,14 @@
+﻿namespace ListasSimplementeLigadas
+{
+    internal class Nodo
+    {
+        public string Valor { get; set; }
+        public Nodo? Enlace { get; set; }
+
+        public Nodo(string valor = "", Nodo? enlace = null)
+        {
+            Valor = valor;
+            Enlace = enlace;
+        }
+    }
+}
