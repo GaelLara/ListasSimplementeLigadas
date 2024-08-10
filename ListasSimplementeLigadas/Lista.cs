8cba1dd4e893cb646e19c4165fb59c37950d83f1
Gael Lara <laragael2003@gmail.com>
2024-08-10 14:09:10 -0700

    BUSCAR,BUSCAR ANTERIOR Y ELIMINAR

--- ListasSimplementeLigadas/Lista.cs
+++ ListasSimplementeLigadas/Lista.cs
@@ -1,4 +1,6 @@
-﻿namespace ListasSimplementeLigadas
+﻿using System.Text;
+
+namespace ListasSimplementeLigadas
 {
     internal class Lista
     {
@@ -36,6 +38,64 @@
             Nodo nodoNuevo = new Nodo(valor);
             nodoActual.Enlace = nodoNuevo;
         }
+
+        public string ObtenerDatos()
+        {
+            StringBuilder datos = new StringBuilder();
+            Nodo nodoActual = _nodoIncial;
+            while (nodoActual.Enlace != null)
+            {
+                nodoActual = nodoActual.Enlace;
+                datos.AppendLine(nodoActual.Valor);
+            }
+            return datos.ToString();
+        }
+
+        public Nodo Buscar(string valor)
+        {
+            Nodo nodoActual = _nodoIncial;
+            while (nodoActual.Enlace != null)
+            {
+                nodoActual = nodoActual.Enlace;
+                if (nodoActual.Valor == valor)
+                {
+                    return nodoActual;
+                }
+            }
+            return null;
+        }
+
+        public Nodo BuscarAnterior(string valor)
+        {
+            Nodo nodoActual = _nodoIncial;
+            while (nodoActual.Enlace != null)
+            {
+                if (nodoActual.Enlace.Valor == valor)
+                {
+                    return nodoActual;
+                }
+                nodoActual = nodoActual.Enlace;
+            }
+            return null;
+        }
+
+        public void Eliminar(string valor) 
+        {
+        Nodo nodoActual = Buscar(valor);
+            if (nodoActual != null) 
+            {
+                Nodo nodoAnterior = BuscarAnterior(valor);
+                if (nodoAnterior != null)
+                {
+                    nodoAnterior.Enlace = nodoActual.Enlace;
+                    nodoActual.Enlace = null;
+                                                                                                                                                                    
+                }
+                    
+            }
+        }
+
+
     }
 }
 
--- ListasSimplementeLigadas/Program.cs
+++ ListasSimplementeLigadas/Program.cs
@@ -4,7 +4,31 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Lista lista = new Lista();
+            lista.Agregar("A");
+            lista.Agregar("B");
+            lista.Agregar("C");
+            lista.Agregar("D");
+            lista.Agregar("E");
+            lista.Agregar("F");
+            Console.WriteLine(lista.ObtenerDatos());
+
+            Console.WriteLine();
+            Console.WriteLine("BUSCANDO A D");
+            Nodo nodoD = lista.Buscar("D");
+            Console.WriteLine(nodoD.Valor);
+
+            Console.WriteLine();
+            Console.WriteLine("BUSCANDO A ANTERIOR A D");
+            Nodo nodoAnteriorD = lista.BuscarAnterior("D");
+            Console.WriteLine(nodoAnteriorD.Valor);
+
+            Console.WriteLine();
+            Console.WriteLine("ELIMINANDO A D");
+            lista.Eliminar("D");
+            Console.WriteLine(lista.ObtenerDatos());
+
+
         }
     }
 }
